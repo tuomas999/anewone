529e7be4396c512cf5e47fb4543fed57e6f91d13
tuomas999 <mindiameladze@gmail.com>
2026-09-14 13:42:52 +0400

    Bump ParkPilot to 0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ge.mindia.parkpilot"
         minSdk = 31
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.3.3"
+        versionCode = 7
+        versionName = "0.4.0"
     }
 
     compileOptions {
